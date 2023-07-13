21bc69bb2007984d5bff054aeb06f5923c8bdbda
Redwan Sony <redwankarimsony1455@gmail.com>
2023-07-13 08:45:19 -0400

    Update download_script.py
    Added the functionality of adding the thumbnail from youtube to the mp3 file

--- download_script.py
+++ download_script.py
@@ -3,6 +3,10 @@ import subprocess
 from multiprocessing import Pool
 from tqdm import tqdm
 
+import eyed3
+from PIL import Image
+import glob
+
 def get_all_urls(playlist_url:str):
     command = ['yt-dlp', '--get-url', '--flat-playlist', playlist_url]
     output = subprocess.check_output(command, universal_newlines=True)
@@ -13,15 +17,16 @@ def get_all_urls(playlist_url:str):
 def download_single_audio(code = None):
     # Prepare the youtube video link
     video_link = f"https://www.youtube.com/watch?v={code}"
-    save_dir = "~/Music/POP-ENG/"
+    save_dir = "~/Music/CarSongs/"
 
     os.makedirs(save_dir, exist_ok=True)
 
     command = ['yt-dlp',
+               '--write-thumbnail',
                '--audio-quality','0',
                '-x',
                '--audio-format', 'mp3',
-               '-o', f'~/Music/POP-ENG/%(title)s.%(ext)s',
+               '-o', f'~/Music/CarSongs/%(title)s.%(ext)s',
                video_link]
     
 
@@ -56,10 +61,35 @@ def get_all_unique_codes(playlist_link_file):
 
 
 
+def embedd_audio(audio_file_path, thumbnail_path = None):
+    # Load the MP3 audio file
+    # audio_file_path = '1.mp3'
+    audio = eyed3.load(audio_file_path)
+
+    # Specify the path to the thumbnail image file
+    if thumbnail_path is None:
+        thumbnail_path = audio_file_path.replace(".mp3", ".webp")
+        img = Image.open(thumbnail_path).convert("RGB")
+        new_thumbnail_path = thumbnail_path.replace(".webp", ".jpg")
+        img.save(new_thumbnail_path)
+
+
+    # Convert The Thumbnail
+
+
+    # Set the thumbnail image
+    audio.tag.images.set(3, open(new_thumbnail_path, 'rb').read(), 'image/jpeg')
+
+    # Save the modified audio file
+    audio.tag.save()
+
+
+
 
 if __name__ == "__main__":
-    codes = get_all_unique_codes("playlist_links.txt")
-    num_threads = 4
+    codes = get_all_unique_codes("car_songs.txt")
+    
+    num_threads = os.cpu_count()-2
 
 
 
@@ -71,10 +101,14 @@ if __name__ == "__main__":
         with open("download_log.txt", "w") as fp:
             print(results, file=fp)
 
-    
 
+    mp3_files = glob.glob("../CarSongs/*.mp3")
 
-
+    for mp3_file in mp3_files:
+        try:
+            embedd_audio(mp3_file)
+        except:
+            print("Something went wrong for ", mp3_file)
 
 
 
